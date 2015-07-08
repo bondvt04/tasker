46b835f8b627bbc09dd41b392890dffe850d290d
bondvt04 <bondvt04@gmail.com>
2015-07-09 00:11:41 +0300

    ejs

--- public/test15-try-to-do-client/modules/notes/controllers/index.js
+++ public/test15-try-to-do-client/modules/notes/controllers/index.js
@@ -19,6 +19,25 @@ define([
         actions : {
             index : function() {
                 console.log(">> notes.controller.actions.index");
+
+                try {
+                    template = new EJS({url: '/modules/notes/views/index/index.ejs'})
+                } catch(e) {
+                    console.error(e);
+                    debugger;
+                }
+
+
+                //data = {
+                //    notes : [
+                //        {text : "hello"},
+                //        {text : "world"},
+                //        {text : "lol"}
+                //    ]
+                //}
+                //
+                //html = new ejs({url: '/modules/notes/views/index/index.ejs'}).render(data);
+                //document.getElementById("content").innerHTML = html;
             },
 
             add : function() {
