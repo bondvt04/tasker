837da27653f4a645222485126990c0f3a86e2453
bondvt04 <bondvt04@gmail.com>
2015-08-01 00:02:32 +0300

    webpack tests

--- test-webpack/webpack.config.js
+++ test-webpack/webpack.config.js
@@ -1,10 +1,25 @@
+var ContextReplacementPlugin = require("webpack/lib/ContextReplacementPlugin");
+
 module.exports = {
     //context: __dirname + "/app",
-    entry: "./src/index",
+    entry: "./index.js",
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
-    }
+    },
+    plugins: [
+        new ContextReplacementPlugin(
+            /aaa/, // change all contexts matching this RegExp
+            /aaa\/hello\.js/,
+            false
+        )
+
+        //new ContextReplacementPlugin(
+        //    /folder[\\\/]modules$/, // change all contexts matching this RegExp
+        //    /^\.\/[^\/]+\/index\.js$/ // Exchange the RegExp with this new RegExp
+        //    // which matches only files in styles, templates and xyz
+        //)
+    ],
     //module: {
     //    loaders: [
     //        { test: /\.css$/, loader: "style!css" }
